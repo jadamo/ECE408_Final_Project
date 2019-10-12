dc0d7a650ddb341dc4d52d50933faafd607fa008
Brian Tang <bt3@linux-02.ews.illinois.edu>
2019-10-12 13:42:45 -0500

    fixed error in new-forward.h and added m2 report

--- ece408_src/new-forward.h
+++ ece408_src/new-forward.h
@@ -26,11 +26,13 @@ void forward(mshadow::Tensor<cpu, 4, DType> &y, const mshadow::Tensor<cpu, 4, DT
     const int H = x.shape_[2];
     const int W = x.shape_[3];
     const int K = k.shape_[3];
+    const int H_out = H - K + 1;
+    const int W_out = W - K + 1;
 
-    for (int b = 0; b < B; b++){     // for each image in the batch 
+    for (int b = 0; b < B; b++){     // for each image in the batch
       for (int m = 0; m < M; m++){   // for each output feature maps
-        for (int h = 0; h < H; h++){  // for each output element
-          for (int w = 0; w < W; w++){
+        for (int h = 0; h < H_out; h++){  // for each output element
+          for (int w = 0; w < W_out; w++){
             y[b][m][h][w] = 0;
             for (int c = 0; c < C; c++){     // sum over all input feature maps
               for (int p = 0; p < K; p++){   // KxK filter
