4e2acb5bb81e2e220ae415329dde6c1e9b09b206
Brian Tang <bt3@linux-a3.ews.illinois.edu>
2019-10-18 13:11:21 -0500

    compiles but wrong correctness

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -13,6 +13,13 @@ namespace op
 __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
 
+  // An example use of these macros:
+  // float a = y4d(0,0,0,0)
+  // y4d(0,0,0,0) = a
+  #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+  #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+  #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+
     /*
     Modify this function to implement the forward pass described in Chapter 16.
     We have added an additional dimension to the tensors to support an entire mini-batch
@@ -22,16 +29,26 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
+    int W_grid = W_out / TILE_WIDTH;
+    int H_grid = H_out / TILE_WIDTH;
     (void)H_out; // silence declared but never referenced warning. remove this line when you start working
     (void)W_out; // silence declared but never referenced warning. remove this line when you start working
 
+    int n = blockIdx.x;
+    int m = blockIdx.y;
+    int h = blockIdx.z / W_grid + threadIdx.y;
+    int w = blockIdx.z % W_grid + threadIdx.x;
+    float value = 0;
+    int c, p, q;
+    for(c = 0; c < C; c++){
+      for(p = 0; p < K; p++){
+        for(q = 0; q < K; q++){
+          value += x4d(n,c,h+p,w+q) * k4d(m,c,p,q);
+        }
+        y4d(n,m,h,w) = value;
+      }
+    }
 
-// An example use of these macros:
-// float a = y4d(0,0,0,0)
-// y4d(0,0,0,0) = a
-#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
-#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
 
 
@@ -60,7 +77,7 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     const int C = x.shape_[1];
     const int H = x.shape_[2];
     const int W = x.shape_[3];
-    const int K = k.shape_[3];
+    const int K = w.shape_[3];
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
 
@@ -68,8 +85,8 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     int H_grid = H_out / TILE_WIDTH;
     int Z = H_grid * W_grid;
     // Set the kernel dimensions
-    dim3 gridDim(TILE_WIDTH, TILE_WIDTH, 1);
-    dim3 blockDim(B,M,Z);
+    dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
+    dim3 gridDim(B,M,Z);
 
     // Call the kernel
     forward_kernel<<<gridDim, blockDim, 0>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
