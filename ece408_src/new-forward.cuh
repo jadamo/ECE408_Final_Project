2aed37efba812e0a3bb36a82c6f7f156782aff6f
Brian Tang <bt3@linux-06.ews.illinois.edu>
2019-10-19 16:52:41 -0500

    m3 submission with report and Kyle's clutch correct code

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -1,4 +1,3 @@
-
 #ifndef MXNET_OPERATOR_NEW_FORWARD_CUH_
 #define MXNET_OPERATOR_NEW_FORWARD_CUH_
 #define TILE_WIDTH 16
@@ -16,9 +15,7 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
   // An example use of these macros:
   // float a = y4d(0,0,0,0)
   // y4d(0,0,0,0) = a
-  #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
-  #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-  #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+
 
     /*
     Modify this function to implement the forward pass described in Chapter 16.
@@ -29,23 +26,33 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-    int W_grid = W_out / TILE_WIDTH;
-    int H_grid = H_out / TILE_WIDTH;
+    const int W_grid = ceil(1.0 * W_out / TILE_WIDTH);
+    //int H_grid = ceil(1.0 * H_out / TILE_WIDTH);
+
+
+    #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+    #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+    #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
     int n = blockIdx.x;
     int m = blockIdx.y;
-    int h = blockIdx.z / W_grid + threadIdx.y;
-    int w = blockIdx.z % W_grid + threadIdx.x;
-    float value = 0;
+    int h = (blockIdx.z / W_grid) * TILE_WIDTH + threadIdx.y;
+    int w = (blockIdx.z % W_grid) * TILE_WIDTH + threadIdx.x;
     int c, p, q;
+    if (h < H_out && w < W_out) {
+
+    float value = 0;
     for(c = 0; c < C; c++){
       for(p = 0; p < K; p++){
         for(q = 0; q < K; q++){
           value += x4d(n,c,h+p,w+q) * k4d(m,c,p,q);
+
+
         }
       }
     }
     y4d(n,m,h,w) = value;
+   }
 
 
 
@@ -79,9 +86,9 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
 
-    float W_grid = ceil(W_out / TILE_WIDTH);
-    float H_grid = ceil(H_out / TILE_WIDTH);
-    float Z = ceil(H_grid * W_grid);
+    const int W_grid = ceil(W_out * 1.0 / TILE_WIDTH);
+    const int H_grid = ceil(H_out * 1.0 / TILE_WIDTH);
+    const int Z = H_grid * W_grid;
     // Set the kernel dimensions
     dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
     dim3 gridDim(B,M,Z);
